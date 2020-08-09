16d066a19d09c7c37dcf742097b70d9667ef5c18
ARTHURPC03 <arthurpimetelcordeiro@gmail.com>
2020-08-09 05:28:41 -0300

    Expo fonts

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,10 +1,53 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable react/jsx-indent */
 import React from 'react'
-import { View } from 'react-native'
+import { Image } from 'react-native'
 
-// import { Container } from './styles';
+import {
+  Container,
+  Banner,
+  Title,
+  TitleBold,
+  ButtonsContainer,
+  ButtonText,
+  ButtonPrimary,
+  ButtonSecondary,
+  TotalConnections,
+} from './styles'
+
+import landingImg from '../../assets/images/landing.png'
+import studyIcon from '../../assets/images/icons/study.png'
+import giveClassesIcon from '../../assets/images/icons/give-classes.png'
+import heartIcon from '../../assets/images/icons/heart.png'
 
 const Landing: React.FC = () => {
-  return <View />
+  return (
+    <Container>
+      <Banner source={landingImg} />
+
+      <Title>
+        Seja bem-vindo,
+        {'\n'}
+        <TitleBold>O que deseja fazer?</TitleBold>
+      </Title>
+
+      <ButtonsContainer>
+        <ButtonPrimary>
+          <Image source={studyIcon} />
+          <ButtonText>Estudar</ButtonText>
+        </ButtonPrimary>
+
+        <ButtonSecondary>
+          <Image source={giveClassesIcon} />
+          <ButtonText>Dar aulas</ButtonText>
+        </ButtonSecondary>
+      </ButtonsContainer>
+
+      <TotalConnections>
+        Total de 285 conexões já realizadas <Image source={heartIcon} />
+      </TotalConnections>
+    </Container>
+  )
 }
 
 export default Landing
